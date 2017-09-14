b7c921ac9136ef1b4cab5105b8c1fa22662bbb37
liuhaifeng <1932736402@qq.com>
2017-09-14 20:48:17 +0800

    Initial commit

---
+++ app/src/main/java/com/baway/baidumarker/MyApp.java
@@ -0,0 +1,17 @@
+package com.baway.baidumarker;
+
+import android.app.Application;
+
+import com.baidu.mapapi.SDKInitializer;
+
+/**
+ * Created by peng on 2017/9/14.
+ */
+
+public class MyApp extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SDKInitializer.initialize(getApplicationContext());
+    }
+}
